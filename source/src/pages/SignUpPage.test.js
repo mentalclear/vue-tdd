4b485f060ea8e068a10a9458fe75183fc2407a09
Dmitrii Kilishek <mentalclear@gmail.com>
2022-06-22 08:05:39 -0400

    Continue: Interacting with Form

--- source/src/pages/SignUpPage.test.js
+++ source/src/pages/SignUpPage.test.js
@@ -2,8 +2,87 @@ import { render, screen } from '@testing-library/vue';
 import SignUpPage from './SignUpPage.vue';
 import '@testing-library/jest-dom';
 
-it('Has Sign Up header', () => {
-  render(SignUpPage);
-  const header = screen.queryByRole('heading', { name: 'Sign Up' });
-  expect(header).toBeInTheDocument();
+describe('Sign Up page', () => {
+  describe('Layout', () => {
+    it('Has Sign Up header', () => {
+      render(SignUpPage);
+      const header = screen.queryByRole('heading', { name: 'Sign Up' });
+      expect(header).toBeInTheDocument();
+    });
+    it('Has 3 input fields', () => {
+      const { container } = render(SignUpPage);
+      const inputCount = container.querySelectorAll('input').length;
+      expect(inputCount).toBe(4);
+    });
+    describe('Username Input', () => {
+      it('Has username input', () => {
+        render(SignUpPage);
+        const input = screen.queryByPlaceholderText('User Name');
+        expect(input).toBeInTheDocument();
+      });
+      it('Has username input label', () => {
+        render(SignUpPage);
+        const input = screen.queryByLabelText('User Name:');
+        expect(input).toBeInTheDocument();
+      });
+    });
+    describe('E-Mail Input', () => {
+      it('Has email input', () => {
+        render(SignUpPage);
+        const input = screen.queryByPlaceholderText('E-Mail');
+        expect(input).toBeInTheDocument();
+      });
+      it('Has email input label', () => {
+        render(SignUpPage);
+        const input = screen.queryByLabelText('User Email:');
+        expect(input).toBeInTheDocument();
+      });
+    });
+    describe('Password Input', () => {
+      it('Has password input', () => {
+        render(SignUpPage);
+        const input = screen.queryByPlaceholderText('Password');
+        expect(input).toBeInTheDocument();
+      });
+      it('Has password input label', () => {
+        render(SignUpPage);
+        const input = screen.queryByLabelText('Password:');
+        expect(input).toBeInTheDocument();
+      });
+      it('Password input has correct type', () => {
+        render(SignUpPage);
+        const input = screen.queryByLabelText('Password:');
+        expect(input.type).toBe('password');
+      });
+    });
+    describe('Repeat Password Input', () => {
+      it('Has Repeat Password input', () => {
+        render(SignUpPage);
+        const input = screen.queryByPlaceholderText('Repeat Password');
+        expect(input).toBeInTheDocument();
+      });
+      it('Has Repeat Password input label', () => {
+        render(SignUpPage);
+        const input = screen.queryByLabelText('Repeat Password:');
+        expect(input).toBeInTheDocument();
+      });
+      it('Repeat Password input has correct type', () => {
+        render(SignUpPage);
+        const input = screen.queryByLabelText('Repeat Password:');
+        expect(input.type).toBe('password');
+      });
+    });
+    describe('Sign Up button', () => {
+      it('Has Sign Up Button', () => {
+        render(SignUpPage);
+        const button = screen.queryByRole('button', { name: 'Sign Up' });
+        expect(button).toBeInTheDocument();
+      });
+      it('Sign Up button To be Disabled by default', () => {
+        render(SignUpPage);
+        const button = screen.queryByRole('button', { name: 'Sign Up' });
+        expect(button).toBeDisabled();
+      });
+    });
+  });
 });
--- source/src/pages/SignUpPage.vue
+++ source/src/pages/SignUpPage.vue
@@ -1,3 +1,34 @@
 <template>
   <h1>Sign Up</h1>
+  <label for="username">
+    User Name:
+    <input
+      id="username"
+      type="text"
+      placeholder="User Name"
+    ></label>
+  <label for="email">
+    User Email:
+    <input
+      id="email"
+      type="text"
+      placeholder="E-Mail"
+    ></label>
+  <label for="password">
+    Password:
+    <input
+      id="password"
+      type="password"
+      placeholder="Password"
+    ></label>
+  <label for="repeat-password">
+    Repeat Password:
+    <input
+      id="repeat-password"
+      type="password"
+      placeholder="Repeat Password"
+    ></label>
+  <button disabled>
+    Sign Up
+  </button>
 </template>

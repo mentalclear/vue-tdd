ee151a88f623b2fda35115db83af5d05051e2e42
Dmitrii Kilishek <mentalclear@gmail.com>
2022-07-01 21:55:59 -0400

    Complete Chapter 6

--- source/src/pages/UserPage.test.js
+++ source/src/pages/UserPage.test.js
@@ -4,12 +4,20 @@ import { setupServer } from 'msw/node';
 import UserPage from './UserPage.vue';
 
 const server = setupServer(
-  rest.get('/api/1.0/users/:id', (req, res, ctx) => res(ctx.status(200), ctx.json({
-    id: 1,
-    username: 'user1',
-    email: 'user1@mail.com',
-    image: null,
-  }))),
+
+  rest.get('/api/1.0/users/:id', (req, res, ctx) => {
+    if (req.params.id === '1') {
+      return res(ctx.status(200), ctx.json({
+        id: 1,
+        username: 'user1',
+        email: 'user1@mail.com',
+        image: null,
+      }));
+    }
+    return res(ctx.status(404), ctx.json({
+      message: 'User not found',
+    }));
+  }),
 );
 
 beforeAll(() => {
@@ -24,17 +32,33 @@ afterAll(() => {
 });
 
 describe('User Page', () => {
-  it('Dislay username when user is found', async () => {
+  const setupUserPage = (id = 1) => {
     render(UserPage, {
       global: {
         mocks: {
-          $route: { params: { id: 1 } },
+          $route: { params: { id } },
         },
       },
     });
+  };
+  it('Dislay username when user is found', async () => {
+    await waitFor(() => {
+      setupUserPage();
+
+      expect(screen.queryByText('user1')).toBeInTheDocument();
+    });
+  });
+  it('Disply spinner while API call is in progress', () => {
+    setupUserPage();
+
+    const spinner = screen.queryByRole('status');
+    expect(spinner).toBeInTheDocument();
+  });
+  it('Disply error message from BE when user is not found', async () => {
+    setupUserPage(100);
 
     await waitFor(() => {
-      expect(screen.queryByText('user1')).toBeInTheDocument();
+      expect(screen.queryByText('User not found')).toBeInTheDocument();
     });
   });
 });
--- source/src/pages/UserPage.vue
+++ source/src/pages/UserPage.vue
@@ -1,24 +1,49 @@
 <template>
   <div data-testid="user-page">
-    <ProfileCard :user="user" />
+    <ProfileCard
+      v-if="!pendingApiCall && !failedResponse"
+      :user="user"
+    />
+    <div
+      v-if="pendingApiCall"
+      class="alert alert-secondary text-center"
+    >
+      <TheSpinner size="normal" />
+    </div>
+    <div
+      v-if="failedResponse"
+      class="alert alert-danger text-center"
+    >
+      {{ failedResponse }}
+    </div>
   </div>
 </template>
 
 <script>
 import { getUserById } from '../api/apiCalls';
 import ProfileCard from '../components/ProfileCard.vue';
+import TheSpinner from '../components/TheSpinner.vue';
 
 export default {
   name: 'UserPage',
-  components: { ProfileCard },
+  components: { ProfileCard, TheSpinner },
   data() {
     return {
       user: {},
+      pendingApiCall: true,
+      failedResponse: undefined,
     };
   },
   async mounted() {
-    const response = await getUserById(this.$route.params.id);
-    this.user = response.data;
+    this.pendingApiCall = true;
+    try {
+      const response = await getUserById(this.$route.params.id);
+      this.user = response.data;
+    } catch (error) {
+      this.failedResponse = error.response.data.message;
+    }
+
+    this.pendingApiCall = false;
   },
 };
 </script>
